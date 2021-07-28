12d591a0b61077d242f067c4a15fea5b023cda98
Beatriz Souza Santos <beatrizsouzasantos1288@gmail.com>
2021-07-28 13:02:50 -0300

    opcoes de visualizacao

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -20,10 +20,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //colocando botao de navegacao na topbar
+        //abrir...
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+
+        
         //adicionando url da pagina
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         //navegacao
         webView.allowsBackForwardNavigationGestures = true
+    }
+    //colocando opcoes para abrir a pagina
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    //titulo a web
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 }
